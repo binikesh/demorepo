493ee38a5ab90978b0004e032cbf8e181686d82c
Binikesh1999 <166592204+binikesh@users.noreply.github.com>
2024-04-28 13:58:28 +0530

    Update myhello.java file

--- myhello.java
+++ myhello.java
@@ -1,6 +1,6 @@
 public class myhello
 {
-	public static void main(Sting[] args)
+	public static void main(String[] args)
 	{
 		//Prints hello World in terminal Window
 		System.out.println("Hello World!!");
